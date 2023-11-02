3ae38c3be2309395ad869441e831d87877c77687
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-11-02 21:59:49 +0200

    memory in Node.js

--- process/process.js
+++ process/process.js
@@ -1,3 +1,9 @@
-const util = require('util');
+// const util = require('util');
+//
+// console.log(util.inspect(process.memoryUsage(), { depth: 0 }));
+//
 
-console.log(util.inspect(process.memoryUsage(), { depth: 0 }));
+const os = require('os');
+
+console.log(`Total Memory: ${os.totalmem()}`);
+console.log(`Free Memory: ${os.freemem()}`);
