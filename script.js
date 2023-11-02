2fdfc735f8c85dd6df284087a87d4265eb3e8572
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-11-02 21:51:44 +0200

    Passing the Buffers underlying ArrayBuffer will create a TypedArray that shares its memory with the Buffer.

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-"use strict";
+// "use strict";
+//
+// const fs = require('fs');
+//
+// try {
+//     const src = fs.readFileSync('1-readFileSync.js', 'utf8');
+//     const lines = src.split('\n').filter(line => line.trim().length > 0);
+//     fs.writeFileSync('1-readFileSync.txt', lines.join('\n'));
+// } catch (error) {
+//     console.error('Произошла ошибка при чтении или записи файла:', error);
+// }
 
-const fs = require('fs');
+const { Buffer } = require('node:buffer');
 
-try {
-    const src = fs.readFileSync('1-readFileSync.js', 'utf8');
-    const lines = src.split('\n').filter(line => line.trim().length > 0);
-    fs.writeFileSync('1-readFileSync.txt', lines.join('\n'));
-} catch (error) {
-    console.error('Произошла ошибка при чтении или записи файла:', error);
-}
+const buf = Buffer.from([1, 2, 3, 4]);
+const uint32array = new Uint32Array(buf);
+
+console.log(uint32array);
+
+// Prints: Uint32Array(4) [ 1, 2, 3, 4 ]
