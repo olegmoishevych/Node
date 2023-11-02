1f11edf39281d6b21abf34330d33a918cf707664
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-11-02 22:13:42 +0200

    Buffer.alloc

--- buffer/buffer.js
+++ buffer/buffer.js
@@ -10,11 +10,16 @@
 //     console.error('Произошла ошибка при чтении или записи файла:', error);
 // }
 
-const { Buffer } = require('node:buffer');
-
-const buf = Buffer.from([1, 2, 3, 4]);
-const uint32array = new Uint32Array(buf);
-
-console.log(uint32array);
+// const { Buffer } = require('node:buffer');
+//
+// const buf = Buffer.from([1, 2, 3, 4]);
+// const uint32array = new Uint32Array(buf);
+//
+// console.log(uint32array);
 
 // Prints: Uint32Array(4) [ 1, 2, 3, 4 ]
+
+'use strict'
+
+const b1 = Buffer.alloc(1024)
+console.log(b1)
