271b9c90e5d2fb185617868aa1e7f2d0fd4563cc
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-11-05 21:50:15 +0200

    Node.js v20.9.0, async_hooks 2 version

--- async_hooks/async_hooks.js
+++ async_hooks/async_hooks.js
@@ -1,26 +1,30 @@
 const async_hooks = require('async_hooks');
 const fs = require('fs');
 
-// Возвращает текущий идентификатор выполнения.
-function eid() {
-    return async_hooks.executionAsyncId();
-}
-
-// Возвращает идентификатор ресурса, который вызвал обратный вызов.
-function tid() {
-    return async_hooks.triggerAsyncId();
-}
-
-// Определяет ресурс, который требует асинхронного отслеживания.
-class MyResource extends async_hooks.AsyncResource {
-    constructor() {
-        super('MyResource');
+// Создаем новый экземпляр AsyncHook.
+const hook = async_hooks.createHook({
+    init(asyncId, type, triggerAsyncId) {
+        fs.writeSync(1, `AsyncHook init: ${asyncId}, type: ${type}, trigger: ${triggerAsyncId}\n`);
     }
+});
+
+// Активируем хуки.
+hook.enable();
+
+// Функция для асинхронной операции, которая принимает callback и идентификатор.
+function doAsyncOperation(name, callback) {
+    fs.writeSync(1, `${name} started, executionAsyncId: ${async_hooks.executionAsyncId()}\n`);
+    setTimeout(() => {
+        fs.writeSync(1, `${name} in setTimeout callback, executionAsyncId: ${async_hooks.executionAsyncId()}\n`);
+        callback();
+    }, 100);
 }
 
-// Инициализирует и запускает ресурс.
-const resource = new MyResource();
-resource.runInAsyncScope(() => {
-    // Теперь мы находимся в асинхронном контексте, созданном MyResource.
-    fs.writeSync(1, `In async scope: ${eid()}\n`);
+// Выполняем две асинхронные операции.
+doAsyncOperation('Operation1', () => {
+    fs.writeSync(1, 'Operation1 callback executed.\n');
+});
+
+doAsyncOperation('Operation2', () => {
+    fs.writeSync(1, 'Operation2 callback executed.\n');
 });
