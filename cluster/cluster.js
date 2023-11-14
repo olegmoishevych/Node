f7add571b4d336776ad5aa31660efb872960be2c
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-11-14 16:54:55 +0200

    Node.js v20.9.0, promise

--- cluster/cluster.js
+++ cluster/cluster.js
@@ -4,46 +4,40 @@ const numCPUs = require('os').cpus().length;
 if (cluster.isMaster) {
     console.log(`Master process is running`);
 
-    // Fork workers.
+    // Fork workers
     for (let i = 0; i < numCPUs; i++) {
+        console.log(`Forking worker ${i+1}`);
         cluster.fork();
     }
-
-    // Отслеживаем выход рабочих процессов
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`Worker process died, pid: ${worker.process.pid}`);
-        // Здесь можно решить, перезапустить рабочий процесс или нет
-    });
-
-    // Функция для остановки всех рабочих процессов
-    function shutdownAllWorkers() {
-        for (const id in cluster.workers) {
-            cluster.workers[id].send('shutdown');
-            cluster.workers[id].disconnect();
-            setTimeout(() => {
-                if (cluster.workers[id]) {
-                    cluster.workers[id].kill();
-                }
-            }, 5000);
-        }
-    }
-
-    // Вы можете вызвать эту функцию когда вам нужно, чтобы остановить все процессы, например через какое-то API или событие.
-
+    // Master process
 } else {
-    // Workers can share any TCP connection
-    // In this case it is an HTTP server
-    console.log(`Worker process started, pid: ${process.pid}`);
+    // Флаг состояния вычисления
+    let keepComputing = true;
 
-    process.on('message', (message) => {
-        if (message === 'shutdown') {
-            // Очистка перед выходом
-            console.log(`Shutting down worker ${process.pid}`);
-            // Закрыть все соединения, таймеры, файлы и т.д., здесь в вашей функции performComputation
-            // ...
-            process.exit(0); // Выход после завершения всех операций
+    // Функция вычисления с возможностью остановки
+    const performComputation = () => {
+        let sum = 0;
+        for (let i = 0; i < 1e8; i++) {
+            if (!keepComputing) {
+                return sum; // Возвращает сумму при получении сигнала остановки
+            }
+            sum += Math.sqrt(i);
+        }
+        return sum;
+    };
+
+    // Обработчик для изменения состояния вычислений
+    process.on('message', (msg) => {
+        if (msg === 'stopComputation') {
+            keepComputing = false; // Устанавливаем флаг в false для остановки вычисления
+        } else if (msg === 'startComputation') {
+            keepComputing = true; // Убедитесь, что вычисление активно перед запуском
+            const result = performComputation();
+            process.send({ result });
         }
     });
+    console.log('res2')
 
-    // Представим, что вычислительная функция вызывается здесь для имитации работы
+    console.log(`Worker process started, pid: ${process.pid}`);
+    // Другие действия рабочего процесса...
 }
